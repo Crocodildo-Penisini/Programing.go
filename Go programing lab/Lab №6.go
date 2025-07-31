7eff3bee1b680c05572da7324c4ba316fbd0d90a
Crocodildo-Penisini <n12341234123412341@gmail.com>
2025-07-31 22:42:53 +0300

    Lab №6.go
    Розробка програми обробки багатовимірних масивів

--- Go programing lab/Lab №6.go
+++ Go programing lab/Lab №6.go
@@ -7,17 +7,12 @@ import "fmt"
 func main() {
 	const rows = 3
 	const cols = 3
-	var matrix [rows][cols]int
-	matrix[0][0] = 9
-	matrix[0][1] = 6
-	matrix[0][2] = 3
-	matrix[1][0] = 2
-	matrix[1][1] = 1
-	matrix[1][2] = 8
-	matrix[2][0] = 5
-	matrix[2][1] = 4
-	matrix[2][2] = 7
-	fmt.Print("Initial array:\n")
+	matrix := [rows][cols]int{
+		{6, 3, 9},
+		{2, 5, 8},
+		{1, 7, 4},
+	}
+	fmt.Print("Початковий масив:\n")
 	for i := 0; i < rows; i++ {
 		for j := 0; j < cols; j++ {
 			fmt.Print(matrix[i][j], " ")
@@ -25,7 +20,7 @@ func main() {
 		fmt.Println()
 	}
 	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
+		for j := 0; j < cols-1; j++ {
 			for k := 0; k < cols-1; k++ {
 				if matrix[i][k] < matrix[i][k+1] {
 					temp := matrix[i][k]
