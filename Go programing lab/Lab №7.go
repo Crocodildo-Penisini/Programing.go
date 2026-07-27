f33e09af1aa0aba87d3f9301a39140f478ade041
✦ᏕᎻᎪᎠᎾᎳ✦ <n12341234123412341@gmail.com>
2026-07-27 20:45:55 +0300

    Lab №7.go

--- Go programing lab/Lab №7.go
+++ Go programing lab/Lab №7.go
@@ -1,1 +1,26 @@
+//Напишіть програму, яка приймає від користувача рядок і замінює всі пробіли на дефіс.
 
+package main
+
+import (
+	"bufio" // - бібліотека для зручного читання тексту з консолі
+	"fmt"
+	"os" // - бібліотека для роботи зі стандартним введенням (stdin)
+)
+
+func main() {
+	var userInput string
+	fmt.Print("Введіть текст: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	userInput = scanner.Text()
+	userInputRunes := []rune(userInput) // - Перетворення рядка у масив символів
+	for index := 0; index < len(userInputRunes); index++ {
+		if userInputRunes[index] == ' ' {
+			userInputRunes[index] = '-'
+		}
+	}
+	userInput = string(userInputRunes)
+
+	fmt.Println("Рядок після заміни:", userInput)
+}
