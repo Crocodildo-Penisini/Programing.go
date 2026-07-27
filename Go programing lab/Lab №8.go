0e3268fc37ee2ccca8543be746b138d796cf1a2c
✦ᏕᎻᎪᎠᎾᎳ✦ <n12341234123412341@gmail.com>
2026-07-27 20:50:49 +0300

    Lab №8.go

--- Go programing lab/Lab №8.go
+++ Go programing lab/Lab №8.go
@@ -1,1 +1,31 @@
+// Напишіть програму, яка приймає від користувача рядок і замінює всі пробіли на дефіс, використовуючи вказівники.
 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func replaceSpace(str *string) { // - str *адреса змінної
+	runes := []rune(*str)
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == ' ' {
+			runes[i] = '-'
+		}
+	}
+	*str = string(runes)
+}
+
+func main() {
+	var str string
+	fmt.Print("Enter string: ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	str = scanner.Text()
+
+	replaceSpace(&str)
+	fmt.Println("Рядок після заміни пробілів на дефіси:", str)
+}
